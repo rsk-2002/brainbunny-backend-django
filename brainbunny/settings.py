ec09eabf2d13a18ff8f3b505d09062b3043c519b
rsk2002 <ravishankerk48@gmail.com>
2024-09-30 16:21:52 +0530

    update allowed hosts

--- brainbunny/settings.py
+++ brainbunny/settings.py
@@ -23,7 +23,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "brainbunny.up.railway.app"]
+
+CSRF_TRUSTED_ORIGINS = ["https://brainbunny.up.railway.app"]
 
 
 # Application definition
